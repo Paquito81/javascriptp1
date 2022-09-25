030a38d7cbf763d75b320e8fb511a39a734302c5
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-09-25 04:22:46 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// function hoursIntoSeconds(hour) {
+//     return seconds = hour * 60 * 60; 
+// }
+// console.log(hoursIntoSeconds(2));
+
+// function calcPerimeter (length, width) {
+//     return 2 * (length + width);
+// }
+
+// console.log(calcPerimeter(10, 20));
+
+// function calcAreaOfTriangle(base, height) {
+//     //Calculate half * base * height
+//     return (0.5) * base * height;
+// }
+
+// console.log(calcAreaOfTriangle(20, 20));
