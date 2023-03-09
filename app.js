d9da29fc0ae60402383349229e67dfe02252a6e1
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2023-03-09 15:27:04 -0700

    learning javascript

--- app.js
+++ app.js
@@ -1,3 +1,86 @@
+// DATA TYPES AND VARIABLES
+
+// STRINGS
+
+// console.log('Hector'[5])
+// console.log('12345'.length)
+// let str = 'Hector'
+// console.log(str.length - 1)
+
+
+//BOOLEAN TYPE
+// TRUE OR FALSE
+
+//NULL & UNDEFINED
+
+//Null = nothing
+//Undefined = never existed
+
+//VARIABLES
+
+//what are variables use for? to store data
+//let
+//const
+
+// let fullName = "hector Palacios"
+// const raining = true
+// let temperature = 30
+// let planet = 'Earth'
+
+// temperature = temperature + 2
+// console.log(temperature)
+
+//FIRST EXERCISE
+
+/** 
+* Converting Temperatures
+*
+* Celsius to Fahrenheit formula
+*
+* F = C x 1.8 + 32
+*/
+
+// let celsius = 10;
+// let fahrenheit = 10 * 1.8 + 32;
+// console.log(fahrenheit);
+
+//EQUALITY
+//"==" and "==="
+
+// let bool = '1' == 1
+// console.log(bool)
+
+
+
+
+//CONDITIONAL
+
+// let subscribed = true
+
+// if (subscribed === true) {
+//     console.log('show the video')
+// }
+// else if (loggedIn === true) {
+//     console.log('tell the user to upgrade')
+
+// }
+// else {
+//     console.log('tell user to log into account')
+// }
+
+let cash = 50
+let price = 40
+
+if (cash > price) {
+console.log("you paid extra - here's {x} dollars change")
+}
+else if (cash === price) {
+    console.log("you paid the exact amount, have a nice day!")
+}
+else {
+    console.log("not enough money you  - you still owe x dollars")
+}
+// console.log('Hello World');
 // function hoursIntoSeconds(hour) {
 //     return seconds = hour * 60 * 60; 
 // }
@@ -62,11 +145,4 @@
 // }
 // console.log(oppositeBoolean(true));
 
-function falsyOrTruthy(elem1, elem2) {
-    if (/* falsy */) {
-        // return the element
-    }
-    else {
-        //return element 2
-    }
-}
+
